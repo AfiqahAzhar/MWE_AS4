7c492443910cd219f9382cc9124f6339f7c1c4d5
Afiqah Azhar <18FTT1715@student.pb.edu.bn>
2020-04-20 03:15:38 +0800

    fav and search commit

--- src/app/district/fav/fav.page.ts
+++ src/app/district/fav/fav.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserdataService } from '../userdata.service';
+import { Router } from '@angular/router';
+import { Room } from '../room.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-fav',
@@ -7,9 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPage implements OnInit {
 
-  constructor() { }
+  listedLoadedRooms: Room[];
+  favourites = [];
+
+  constructor(public userData: UserdataService,
+              public router: Router,
+              public storage: Storage) { }
 
   ngOnInit() {
   }
+
+
 
 }
