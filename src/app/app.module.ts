5f1b727eb0a05169a609d4a21c500e346bca3b31
Bayhaqqi <18ftt1707@student.pb.edu.bn>
2020-04-20 16:53:03 +0800

    image-gallery and camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule } from '@angular/common/http';
 
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,8 +21,9 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Storage
+    Storage,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/district/image-gallery/image-gallery.page.ts
+++ src/app/district/image-gallery/image-gallery.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-gallery',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageGalleryPage implements OnInit {
 
-  constructor() { }
+  loading;
+  currentImage;
+  selectedPhoto;
+  storageRef = firebase.storage().ref();
+  starsRef = this.storageRef.child('photos/gallery1.jpg');
 
-  ngOnInit() {
+constructor(private camera: Camera,
+            private loadingCtrl: LoadingController) { }
+
+ngOnInit() {
   }
 
+grabPicture() {
+    const options: CameraOptions = {
+      quality: 50,
+      targetHeight: 200,
+      targetWidth: 200,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.loading = this.loadingCtrl;
+      this.loading.present();
+
+      this.selectedPhoto = this.dataURItoBlob('data:image/jpeg;base64,' + imageData);
+
+      this.upload();
+    }, (err) => {
+      console.log('error', err);
+    });
+  }
+
+dataURItoBlob(dataURI) {
+    const binary = atob(dataURI.split(',')[1]);
+    const array = [];
+    for (let i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+  }
+
+upload() {
+    if (this.selectedPhoto) {
+      const uploadTask = firebase.storage().ref().child('photos/gallery')
+      .put(this.selectedPhoto);
+      uploadTask.then(this.onSuccess, this.onError);
+    }
+  }
+
+onSuccess = snapshot => {
+    this.currentImage = snapshot.downloadURL;
+    this.loading.dismiss();
+  }
+
+onError = error => {
+    console.log('error', error);
+    this.loading.dismiss();
+  }
 }
